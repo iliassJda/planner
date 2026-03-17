2487a27c5e50f8529cd4fdcf145f44ba1ab8f040
iliassJda <150142617+iliassJda@users.noreply.github.com>
2026-03-17 14:21:25 +0100

    added the start for the dropzone

--- app/(admin)/admin/maker/page.tsx
+++ app/(admin)/admin/maker/page.tsx
@@ -1,6 +1,12 @@
-import { getAllAvailability, getAllWeeks } from "@/action/supabase";
+// import { getAllAvailability, getAllWeeks } from "@/action/supabase";
+import DropArea from "./drop-area";
 // import MakerClient from "./maker-client";
 
 export default async function MakerPage() {
-	return <div className="w-full h-full">test</div>;
+	return (
+		<div className="w-full h-full">
+			<h1 className="text-2xl font-bold md:text-3xl">Planner Making</h1>
+			<DropArea />
+		</div>
+	);
 }
